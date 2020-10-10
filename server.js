8c9f9faf9180e7ab6ede85154325bc6d2250b4b7
Ver <vpasamic22@gmail.com>
2020-10-09 19:25:40 -0700

    understands public folder

--- server.js
+++ server.js
@@ -7,18 +7,19 @@ const fs = require("fs");
 // Sets up the Express App
 // =============================================================
 const app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = 3000;
 
 // Sets up the Express app to handle data parsing
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //html routes
 app.get("/notes/", (req,res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 //API ROUTES
