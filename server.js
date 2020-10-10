f7394e474b27b5e9677ccda7666ada915b2f4a40
Ver <vpasamic22@gmail.com>
2020-10-09 17:38:40 -0700

    server now listens

---
+++ server.js
@@ -0,0 +1,22 @@
+// Dependencies
+// =============================================================
+const express = require("express");
+const path = require("path");
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
