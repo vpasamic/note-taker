6360e24e8dd409ceddd9947cf7f239341749f76d
Ver <vpasamic22@gmail.com>
2020-10-09 19:43:37 -0700

    added api post request for new notes)

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 var PORT = 3000;
 
 // Sets up the Express app to handle data parsing
+app.use(express.static("db"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -24,17 +25,18 @@ app.get("/", (req,res) => {
 
 //API ROUTES
 app.get("/api/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "/db/db.json"));
+    res.sendFile(path.join(__dirname, "./db/db.json"));
 });
 
 app.post("/api/notes", function(req, res) {
-      let savednotes = fs.readFileSync("db/db.json", "utf8");
+      let savednotes = fs.readFileSync("./db/db.json", "utf8");
       savednotes = JSON.parse(savednotes);
       let newnote = req.body
       newnote.id = savednotes.length+1;
       newnote.id = JSON.stringify(newnote.id)
-      savednotes.push(req.body); 
-      console.log(savednotes)
+      savednotes.push(newnote); 
+      fs.writeFileSync("./db/db.json", JSON.stringify(savednotes))
+      
 });
 
 app.listen(PORT, function() {
