201d4557123a2c28566a6d01adb1e8c759e196be
Ver <vpasamic22@gmail.com>
2020-10-09 19:03:46 -0700

    put in fresh db and public folders

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 // =============================================================
 const express = require("express");
 const path = require("path");
-const fs = require("fs")
+const fs = require("fs");
 
 // Sets up the Express App
 // =============================================================
@@ -15,10 +15,10 @@ app.use(express.json());
 
 //html routes
 app.get("/notes/", (req,res) => {
-    res.sendFile(path.join(__dirname, "notes.html"));
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 //API ROUTES
@@ -26,8 +26,16 @@ app.get("/api/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/db/db.json"));
 });
 
-
-
+app.post("/api/notes", function(req, res) {
+   
+      
+      let savednotes = fs.readFileSync("./Develop/db/db.json", "utf8");
+      savednotes = JSON.parse(savednotes);
+      let newnote = req.body
+      newnote.id = savednotes.length;
+      savednotes.push(req.body); 
+      console.log(savednotes)
+});
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
